c5c6a84064dc4fd9f2fa5438123aca6e043166cf
olaworks <93895857+olaworks@users.noreply.github.com>
2022-04-02 21:19:41 -0700

    Update homework-js.js

--- homework-js.js
+++ homework-js.js
@@ -1,5 +1,5 @@
 // HOMEWORK PAGE 2 FORMS
-function process() {
+function process1() {
 
     number1 = document.forms["IntegerForm"].elements["integer1"].value;
     number2 = document.forms["IntegerForm"].elements["integer2"].value;
@@ -21,7 +21,9 @@ function process() {
     document.forms["IntegerForm"].elements["productResult"].value = product;
     document.forms["IntegerForm"].elements["smallestNumber"].value = min;
     document.forms["IntegerForm"].elements["largestNumber"].value = max;
+  }
 
+function process2(){
       test5_number1 = document.forms["Test5Form"].elements["test5_integer1"].value;
       test5_number2 = document.forms["Test5Form"].elements["test5_integer2"].value;
       test5_number3 = document.forms["Test5Form"].elements["test5_integer3"].value;
@@ -106,46 +108,59 @@ function process() {
 
       document.forms["Test5Form"].elements["negativeintResult"].value = displaynegativeint;
 
+
+
+
     }
 
-    //HOMEWORK PAGE 3 FORMS
-  function process() {
-      hwAvgGrade        = document.forms["GetFinalGradeForm"].elements["hwAvg"].value;
-      midExamGrade      = document.forms["GetFinalGradeForm"].elements["midExam"].value;
-      finalExamGrade    = document.forms["GetFinalGradeForm"].elements["finalExam"].value;
-      particpationGrade = document.forms["GetFinalGradeForm"].elements["particpation"].value;
+    function process3(){
+      //HOMEWORK PAGE 3 FORMS
 
-      hg = parseInt(hwAvgGrade);
-      mg = parseInt(midExamGrade);
-      fg = parseInt(finalExamGrade);
-      pg = parseInt(particpationGrade);
+        hwAvgGrade        = document.forms["GetFinalGradeForm"].elements["hwAvg"].value;
+        midExamGrade      = document.forms["GetFinalGradeForm"].elements["midExam"].value;
+        finalExamGrade    = document.forms["GetFinalGradeForm"].elements["finalExam"].value;
+        particpationGrade = document.forms["GetFinalGradeForm"].elements["particpation"].value;
 
-      //make sure all are integers AND all between 0-100
+        hg = parseInt(hwAvgGrade);
+        mg = parseInt(midExamGrade);
+        fg = parseInt(finalExamGrade);
+        pg = parseInt(particpationGrade);
 
-      tfa = (0.5 * hg) + (0.2 * mg) + (0.2 * fg) + (0.1 * pg);
+        if (hg < 0 || hg > 100 || mg < 0 || mg > 100 || fg < 0 || fg > 100 || pg < 0 || pg > 100 ) {
+         window.alert("Please only enter numbers between 0 and 100.");
+        }
 
-      document.forms["GetFinalGradeForm"].elements["theFinalAverage"].value = tfa;
+        /*if (isNaN(hg) || isNaN(mg) || (isNaN(fg) || (isNaN(pg)) {
+          window.alert("Please only enter Numbers, that are between 0 and 100..");
+        }*/
 
-      /*letterA = "A";
-      letterB = "B";
-      letterC = "C";
-      letterD = "D";
-      letterF = "F";
+        tfa = (0.5 * hg) + (0.2 * mg) + (0.2 * fg) + (0.1 * pg);
 
-       if (tfa >= 90 && tfa <= 100) {
-         document.forms["GetFinalGradeForm"].elements["theFinalGrade"].value = letterA;
-       }
-         if (tfa >= 80 && tfa <= 89) {
-         document.forms["GetFinalGradeForm"].elements["theFinalGrade"].value = letterB;
-       }
-       if (tfa >= 70 && tfa <= 79) {
-         document.forms["GetFinalGradeForm"].elements["theFinalGrade"].value = letterC;
-       }
-       if (tfa >= 60 && tfa <= 69) {
-         document.forms["GetFinalGradeForm"].elements["theFinalGrade"].value = letterD;
-       }
-       if (tfa >= 0 && tfa <=59) {
-         document.forms["GetFinalGradeForm"].elements["theFinalGrade"].value = letterF;
-       }*/
-    
+        document.forms["GetFinalGradeForm"].elements["theFinalAverage"].value = tfa;
+
+        letterA = "A";
+        letterB = "B";
+        letterC = "C";
+        letterD = "D";
+        letterF = "F";
+
+         if (tfa >= 90) {
+           document.forms["GetFinalGradeForm"].elements["theFinalGrade"].value = letterA;
+         }
+         else if (tfa >= 80) {
+           document.forms["GetFinalGradeForm"].elements["theFinalGrade"].value = letterB;
+         }
+         else if (tfa >= 70) {
+           document.forms["GetFinalGradeForm"].elements["theFinalGrade"].value = letterC;
+         }
+         else if (tfa >= 60) {
+           document.forms["GetFinalGradeForm"].elements["theFinalGrade"].value = letterD;
+           // display student must retake
+         }
+         else {
+           document.forms["GetFinalGradeForm"].elements["theFinalGrade"].value = letterF;
+           // display student must retake
+         }
+
     }
+
